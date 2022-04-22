549a77d8c6c848c6ba1c93296dbf9dc908944da8
doctordp <dominicdemeterca@gmail.com>
2022-04-22 13:33:04 +0200

    instanciar objeto y mostrar

--- ejercicio 1/src/ejercici1.java
+++ ejercicio 1/src/ejercici1.java
@@ -4,6 +4,9 @@ public class ejercici1 {
 	public static void main(String[] args) {
 		System.out.println("Listado de alumnos 1º DAW");
 		System.out.println("Alumno 1");
+		
+		Alumno alumno1=new Alumno("ruben","portuguez","502235273F");
+		System.out.println(alumno1);
 
 	}
 
