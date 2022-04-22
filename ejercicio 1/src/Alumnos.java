09b61607fd384ea83efe33550920d2270006a906
doctordp <dominicdemeterca@gmail.com>
2022-04-22 13:26:47 +0200

    añadir constructor

--- ejercicio 1/src/Alumnos.java
+++ ejercicio 1/src/Alumnos.java
@@ -2,4 +2,10 @@
 public class Alumnos {
 	//atributos
 	private String nombre,apellido,dni;
+	//constructores 
+	public Alumnos(String a,String b, String c) {
+		this.nombre=a;
+		this.apellido=b;
+		this.dni=c;
+	}
 }
